3f095048f008662e954580bf4a569950dd3c7777
GeorgeFatan <george.fatan@ulbsibiu.ro>
2025-04-25 18:53:15 +0300

    FixedHarvestBug
    -> Cum am pus 2 terenuri pe scena, practic am dat copy paste la teren .. iar cuburile din parentu Terren2, aveau acelasi nume cu cele din Terren. => la recoltare conflict.. astepta ca si C1 de la T2 sa fie plantat si in S3

--- Assets/Scripts/PlantAndGrow Scripts/PlantScript.cs
+++ Assets/Scripts/PlantAndGrow Scripts/PlantScript.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class PlantScript : MonoBehaviour
 {
@@ -11,8 +12,9 @@ public class PlantScript : MonoBehaviour
     private int lastDaysPassed = 0;
 
     public Animator playerAnimator;
+    private bool isAnimating = false;
     public bool isPlayerInPlantingZone = false;
-
+    
     private PlantData selectedPlantData;
     private InventorySystem inventorySystem;
 
@@ -43,7 +45,7 @@ public class PlantScript : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.P) && !isAnimating)
         {
             if (isPlayerInPlantingZone)
             {
@@ -61,9 +63,11 @@ public class PlantScript : MonoBehaviour
                     return;
                 }
 
+                isAnimating = true;
                 PlayPlantingAnimation();
-                PlantOnCube();
-                inventorySystem.RemoveItemFromSlot(selectedPlantData.seedTexture);
+                StartCoroutine(WaitForAnimation());
+               /* PlantOnCube();
+                inventorySystem.RemoveItemFromSlot(selectedPlantData.seedTexture);*/
             }
             else
             {
@@ -83,6 +87,20 @@ public class PlantScript : MonoBehaviour
             AdvanceGrowthStage();
         }
     }
+
+    // Corutina = multitasking = permite suspendarea si reluarea executiei.
+    IEnumerator WaitForAnimation()
+    {
+        float animationTime = playerAnimator.GetCurrentAnimatorStateInfo(0).length;
+        yield return new WaitForSeconds(animationTime); // asteptam sa se termina animatia care este in desfasurare
+        PlantOnCube(); // Plasam prefabu dupa terminarea animatiei
+        inventorySystem.RemoveItemFromSlot(selectedPlantData.seedTexture);
+
+        isAnimating=false;
+
+    }
+
+
 
     void PlayPlantingAnimation()
     {
