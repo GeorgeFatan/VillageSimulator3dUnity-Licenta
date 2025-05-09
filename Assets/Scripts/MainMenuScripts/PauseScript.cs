f5f770aa76552d293800b48ea9da5194d3656445
GeorgeFatan <george.fatan@ulbsibiu.ro>
2025-05-09 21:28:50 +0300

    MainMenu implementation

---
+++ Assets/Scripts/MainMenuScripts/PauseScript.cs
@@ -0,0 +1,74 @@
+using StarterAssets;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(!isPaused)
+            {
+                EscPause();
+            }
+            else
+            {
+                EscResume();
+            }
+        }
+    }
+
+    void EscPause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+
+        StarterAssetsInputs playerInputs = FindObjectOfType<StarterAssetsInputs>();
+        if(playerInputs != null)
+        {
+            playerInputs.cursorInputForLook = false;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        
+    }
+
+    public void EscResume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        isPaused = false;
+
+        StarterAssetsInputs playerInputs = FindObjectOfType<StarterAssetsInputs>();
+        if (playerInputs != null)
+        {
+            playerInputs.cursorInputForLook = true;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = true;
+        }
+
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+}
